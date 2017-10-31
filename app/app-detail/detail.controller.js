3249880e279fa89cd62e4e1618b4b1c1501a2a84
ogonzalr <ligerzxvn@hotmail.com>
2017-10-31 11:22:12 -0600

    adding files.

---
+++ app/app-detail/detail.controller.js
@@ -0,0 +1,44 @@
+﻿angular.module('detail', ['ui.bootstrap']);
+function AccordionDemoCtrl($scope) {
+	
+  $scope.groups = [
+    {
+      title: "Dynamic Group Header - 1",
+      content: "Dynamic Group Body - 1"
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2"
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+  
+    $scope.pro = [
+      {
+        a : "G",
+        b : "123",
+        c : "S1",
+        d : "D6",
+      },
+         {
+        a : "R",
+        b : "456",
+        c : "S2",
+        d : "D5",
+      },
+         {
+        a : "G",
+        b : "789",
+        c : "S3",
+        d : "D4",
+         },
+    ];
+    $scope.orderProp="d";                  
+  
+}
